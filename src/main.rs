990cbb73b11db906663453e7d270bce106424abc
dominiclet <dominic.let@hotmail.com>
2022-12-17 14:18:06 +0800

    implemented send_message and get_updates, implemented echo loop

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-use std::env;
+use core::time;
+use std::{env, thread::sleep};
 
 use telegram_apis::TelegramClient;
 
 mod telegram_apis;
 mod types;
 
+#[macro_use] extern crate log;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
+
     let bot_token = match env::var("BOT_TOKEN") {
         Ok(token) => token,
         Err(error) => panic!("Error retrieving BOT_TOKEN environment variable: {:?}", error)
     };
-    let telegram_client = TelegramClient::new(bot_token);
+    let mut telegram_client = TelegramClient::new(bot_token);
 
-    let updates = telegram_client.get_updates().await?;
+    info!("Starting update loop");
+    loop {
+        let updates = telegram_client.get_updates().await?;
 
-    println!("{:?}", updates);
-    Ok(())
+        println!("{:?}", updates);
+
+        for update in updates {
+            telegram_client.send_message(update.message.chat.id, 
+                                         update.message.text).await?;
+        }
+
+        sleep(time::Duration::from_secs(2));
+    }
 }
 
--- src/telegram_apis.rs
+++ src/telegram_apis.rs
@@ -1,31 +1,92 @@
-use crate::types::{GetUpdatesResp, UpdateList};
+use std::collections::HashMap;
+
+use crate::types::{GetUpdatesResp, UpdateList, SendMessageReq};
 
 const TELEGRAM_DOMAIN: &str = "https://api.telegram.org";
 
 pub struct TelegramClient {
     http_client: reqwest::Client,
-    token: String
+    token: String,
+    update_offset: Option<i32>
 }
 
 impl TelegramClient {
     pub fn new(token: String) -> TelegramClient {
         TelegramClient {
             http_client: reqwest::Client::new(),
-            token 
+            token,
+            update_offset: None
         }
     }
 
-    fn construct_endpoint(&self, method: &str) -> String {
-        format!("{}/bot{}/{}", TELEGRAM_DOMAIN, self.token, method)
+    fn construct_endpoint(&self, method: &str, params: Option<HashMap<String, i32>>) -> String {
+        let endpoint_string: String;
+        match params {
+            Some(params) => {
+                let mut parameters: Vec<String> = Vec::new();
+                for (key, value) in params {
+                    parameters.push(format!("{}={}", key, value));
+                }
+                let param_string = parameters.join("&");
+                endpoint_string = format!("{}/bot{}/{}?{}", TELEGRAM_DOMAIN, self.token, method, param_string);
+            }
+
+            None => {
+                endpoint_string = format!("{}/bot{}/{}", TELEGRAM_DOMAIN, self.token, method);
+            }
+        }
+
+        endpoint_string
     }
 
-    pub async fn get_updates(&self) -> Result<UpdateList, reqwest::Error> {
-        let resp = reqwest::get(self.construct_endpoint("getUpdates")).await?;
+    pub async fn get_updates(&mut self) -> Result<UpdateList, Box<dyn std::error::Error>> {
+        let endpoint: String;
+
+        match self.update_offset {
+            Some(update_offset) => {
+                let param_map = HashMap::from([
+                    (String::from("offset"), update_offset)
+                ]);
+                endpoint = self.construct_endpoint("getUpdates", Some(param_map));
+            }
+
+            None => {
+                endpoint = self.construct_endpoint("getUpdates", None);
+            }
+        }
+
+        let resp = self.http_client.get(endpoint)
+            .send().await?;
 
         let get_updates_resp = resp.json::<GetUpdatesResp>().await?;
+
+        // Handle not OK response
+        if get_updates_resp.ok == false {
+            return Err("getUpdates did not return a successful response.".into());
+        }
+
+        // Update offset so that previous updates are no longer returned
+        let mut max_update_id: i32 = 0;
+        for update in &get_updates_resp.result {
+            if update.update_id > max_update_id {
+                max_update_id = update.update_id;
+            }
+        }
+        self.update_offset = Some(max_update_id + 1);
 
         Ok(get_updates_resp.result)
     }
 
+    pub async fn send_message(&self, chat_id: i64, text: String) -> Result<(), Box<dyn std::error::Error>> {
+        let message_req = SendMessageReq {
+            chat_id,
+            text
+        };
+
+        self.http_client.post(self.construct_endpoint("sendMessage", None))
+            .json(&message_req).send().await?;
+
+        Ok(())
+    }
 }
 
